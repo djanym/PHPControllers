da23119b054e307dccb4e78fe13a38cd1faa1524
Nael Concescu <djanym@gmail.com>
2022-01-06 18:59:15 +0300

    error page message added

--- packages/BaseController.php
+++ packages/BaseController.php
@@ -11,11 +11,18 @@ class BaseController
 {
     /**
      * @param string $tpl View name. For index.blade.php you should use $tpl = 'index'. Referrer to blade templating syntax.
-     * @param array $vars
+     * @param array|object $vars
      * @throws Exception
      */
     public static function display($tpl, $vars = [], $return = false)
     {
+        $vars = parse_args(
+            $vars,
+            [
+                'menu_active' => null,
+            ]
+        );
+
         $views = TPLPATH;
         $cache = TPLPATH . '/cache';
         $blade = new BladeExt($views, $cache, BladeOne::MODE_DEBUG); // MODE_DEBUG allows to pinpoint troubles.
@@ -37,9 +44,15 @@ class BaseController
         self::display('page-403');
     }
 
-    public static function display404()
+    public static function display404($page_title = null, $error_message = null)
     {
+        if (!$page_title) {
+            $error_message = __('Page not found');
+        }
+        if (!$error_message) {
+            $error_message = __('No page was found fot this URL.');
+        }
         header(get_status_header_message(404), true, 404);
-        self::display('page-404');
+        self::display('page-404', ['page_title' => $page_title, 'error_message' => $error_message]);
     }
 }
